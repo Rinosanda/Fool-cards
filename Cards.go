e1a4fa076960630739bddc3bea2f0f24f8e7f080
Rinosanda <muhetdinofff.rinat47@gmail.com>
2025-08-10 01:12:43 +0300

    Update Cards.go

--- Cards.go
+++ Cards.go
@@ -1,132 +1,102 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-// Карта
 type Card struct {
-    suit  string
-    value string
+	Suit  string
+	Value string
 }
 
-// Игрок
-type Player struct {
-    name       string
-    hand       []Card
-    isAttacker bool
+var suits = []string{"Пики", "Червы", "Бубны", "Трефы"}
+var values = []string{"6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"}
+
+func createDeck() []Card {
+	var deck []Card
+	for _, suit := range suits {
+		for _, value := range values {
+			deck = append(deck, Card{Suit: suit, Value: value})
+		}
+	}
+	return deck
 }
 
-// Колода
-type Deck []Card
-
-// Создание колоды
-func NewDeck() Deck {
-    suits := []string{"Черви", "Бубны", "Трефы", "Пики"}
-    values := []string{"6", "7", "8", "9", "10", "В", "Д", "К", "Т"}
-    
-    var deck Deck
-    for _, suit := range suits {
-        for _, value := range values {
-            deck = append(deck, Card{suit: suit, value: value})
-        }
-    }
-    return deck
+func shuffleDeck(deck []Card) []Card {
+	rand.Seed(time.Now().UnixNano())
+	for i := len(deck) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		deck[i], deck[j] = deck[j], deck[i]
+	}
+	return deck
 }
 
-// Перемешивание колоды
-func (d *Deck) Shuffle() {
-    rand.Seed(time.Now().UnixNano())
-    rand.Shuffle(len(*d), func(i, j int) { (*d)[i], (*d)[j] = (*d)[j], (*d)[i] })
+func dealCards(deck []Card) ([]Card, []Card, []Card) {
+	playerHand := deck[:6]
+	computerHand := deck[6:12]
+	remainingDeck := deck[12:]
+	return playerHand, computerHand, remainingDeck
 }
 
-// Раздача карт
-func DealCards(deck Deck, players []Player) {
-    for i := 0; i < 6; i++ {
-        for j, player := range players {
-            players[j].hand = append(player.hand, deck[0])
-            deck = deck[1:]
-        }
-    }
+func printHand(name string, hand []Card) {
+	fmt.Printf("%s: ", name)
+	for i, card := range hand {
+		fmt.Printf("[%d: %s %s] ", i, card.Value, card.Suit)
+	}
+	fmt.Println()
 }
 
-// Функция для удаления карты из руки
-func removeCard(hand []Card, card Card) []Card {
-    newHand := []Card{}
-    for _, c := range hand {
-        if c.value != card.value || c.suit != card.suit {
-            newHand = append(newHand, c)
-        }
-    }
-    return newHand
+func playerTurn(hand []Card) ([]Card, Card) {
+	printHand("Ваши карты", hand)
+	var index int
+	for {
+		fmt.Print("Выберите номер карты для хода: ")
+		_, err := fmt.Scanf("%d\n", &index)
+		if err == nil && index >= 0 && index < len(hand) {
+			break
+		}
+		fmt.Println("Неверный ввод, попробуйте снова.")
+	}
+	card := hand[index]
+	hand = append(hand[:index], hand[index+1:]...)
+	return hand, card
 }
 
-// Функция для проверки возможности хода
-func canPlayCard(attacker *Player, defender *Player, card Card) bool {
-    // Простая проверка - можно ходить любой картой
-    return true
-}
-
-// Функция для выполнения хода игрока
-func playerTurn(player *Player, opponent *Player) {
-    fmt.Printf("Ход игрока %s\n", player.name)
-    // Здесь логика выбора карты игроком
-    chosenCard := player.hand[0] // Временная реализация
-    fmt.Printf("Выбранная карта: %s %s\n", chosenCard.value, chosenCard.suit)
-    
-    // Проверка возможности хода
-    if canPlayCard(player, opponent, chosenCard) {
-        // Выполнение хода
-        player.hand = removeCard(player.hand, chosenCard)
-        opponent.hand = append(opponent.hand, chosenCard)
-    } else {
-        fmt.Println("Некорректный ход!")
-    }
-}
-
-// Функция для выполнения хода компьютера
-func computerTurn(computer *Player, human *Player) {
-    fmt.Println("Ход компьютера")
-    // Простая реализация ИИ
-    chosenCard := computer.hand[0] // Компьютер ходит первой картой
-    fmt.Printf("Компьютер выбрал карту: %s %s\n", chosenCard.value, chosenCard.suit)
-    
-    // Выполнение хода
-    computer.hand = removeCard(computer.hand, chosenCard)
-    human.hand = append(human.hand, chosenCard)
-}
-
-// Основной игровой цикл
-func gameLoop(players []Player) {
-    attacker := &players[0] // Первый атакующий
-    defender := &players[1]
-    
-    for {
-        // Проверка на победу
-        if len(attacker.hand) == 0 {
-            fmt.Printf("Победил %s!\n", defender.name)
-            break
-        }
-        if len(defender.hand) == 0 {
-            fmt.Printf("Победил %s!\n", attacker.name)
-            break
-        }
-        
-        // Ход атакующего
-        if attacker.name == "Игрок" {
-            playerTurn(attacker, defender)
-        } else {
-            computerTurn(attacker, defender)
-        }
-        
-        // Смена ролей
-        attacker, defender = defender, attacker
-    }
+func computerTurn(hand []Card) ([]Card, Card) {
+	index := rand.Intn(len(hand))
+	card := hand[index]
+	hand = append(hand[:index], hand[index+1:]...)
+	fmt.Printf("Компьютер ходит картой: %s %s\n", card.Value, card.Suit)
+	return hand, card
 }
 
 func main() {
-    // Создаем колоду и перемешиваем
-    deck := NewDeck()
-    deck.Shuffle()
+	deck := shuffleDeck(createDeck())
+
+	playerHand, computerHand, remainingDeck := dealCards(deck)
+	trump := remainingDeck[len(remainingDeck)-1].Suit
+	fmt.Println("Козырь:", trump)
+
+	for len(playerHand) > 0 && len(computerHand) > 0 {
+		// Игрок ходит
+		var playedCard Card
+		playerHand, playedCard = playerTurn(playerHand)
+		fmt.Printf("Вы выложили карту: %s %s\n", playedCard.Value, playedCard.Suit)
+
+		// Компьютер отвечает
+		computerHand, _ = computerTurn(computerHand)
+
+		fmt.Println("Раунд завершён.")
+		fmt.Println()
+	}
+
+	fmt.Println("Игра окончена.")
+	if len(playerHand) == 0 {
+		fmt.Println("Вы победили!")
+	} else {
+		fmt.Println("Компьютер победил!")
+	}
+}
+
